838d167d9a7d9d542d41003438005a0cee926852
M. Castro <matheuscastro070@gmail.com>
2025-02-26 18:16:28 -0300

    tailwind works

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    future: {
+      legacy: true,
+    },
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
